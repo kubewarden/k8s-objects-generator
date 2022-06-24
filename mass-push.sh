72abac9b707d3707a164b9c14525c98171ea9fd5
Flavio Castelli <fcastelli@suse.com>
2022-06-24 13:51:32 +0200

    Add mass push script
    This is useful to push all the changes inside of the k8s-objects repo to GitHub
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

---
+++ mass-push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A simple bash scripts that generates k8s types for a series
+# of kubernetes releases.
+# The generated objects are then added to a local checkout of the
+# github.com/kubewarden/k8s-objects repository
+
+set -ex
+
+GIT_DIR=~/hacking/kubernetes/kubewarden/k8s-objects
+
+for KUBEMINOR in {14..24}
+do
+  echo ==================================
+  echo PROCESSING KUBERNETES 1.$KUBEMINOR
+  echo ==================================
+
+  BRANCH=release-1.$KUBEMINOR
+
+  cd $GIT_DIR
+  git checkout $BRANCH
+  git push origin $BRANCH
+done
+
+git push --tags
