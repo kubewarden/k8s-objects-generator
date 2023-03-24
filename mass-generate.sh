16490b9f8e5e629a8a698931ddf18168dd432143
Dmitry Volodin <dmvolod@gmail.com>
2023-03-24 12:11:25 +0300

    Add ability to specify Kubernetes versions range
    Signed-off-by: Dmitry Volodin <dmvolod@gmail.com>

--- mass-generate.sh
+++ mass-generate.sh
@@ -10,6 +10,10 @@ set -e
 OUT_DIR=~/k8s-data-types
 GIT_DIR=~/checkout/kubernetes/kubewarden/k8s-objects
 
+# Able to define Kubernetes versions range (for testing)
+KUBERNETES_VERSION_MIN="${KUBEMINOR_MIN:-14}"
+KUBERNETES_VERSION_MAX="${KUBEMINOR_MAX:-26}"
+
 while [[ $# -gt 0 ]]; do
   case $1 in
     -m|--message)
@@ -43,10 +47,10 @@ if [ -z "$GIT_COMMIT_MSG_FILE" ]; then
   exit 1
 fi
 
-for KUBEMINOR in {14..26}
+for KUBEMINOR in $(eval "echo {$KUBERNETES_VERSION_MIN..$KUBERNETES_VERSION_MAX}");
 do
   echo ==================================
-  echo PROCESSING KUBERNETES 1.$KUBEMINOR
+  echo PROCESSING KUBERNETES "1.$KUBEMINOR"
   echo ==================================
 
   ./k8s-objects-generator -kube-version "1.$KUBEMINOR" -o "$OUT_DIR"
@@ -54,23 +58,25 @@ do
   BRANCH=release-1.$KUBEMINOR
 
   cd "$GIT_DIR"
-  git fetch origin
+  git checkout main
+  upstream=$(git rev-parse --abbrev-ref --symbolic-full-name '@{u}' | awk -F "/" '{print $1}')
+  git fetch "$upstream"
 
   if [ $((n=$(git branch -r | grep -wic "$BRANCH"))) -gt 0 ]; then
-    git checkout $BRANCH
-    git rebase origin/$BRANCH $BRANCH
+    git checkout "$BRANCH"
+    git rebase "$upstream"/"$BRANCH" "$BRANCH"
 
     n=$(git tag | grep -wic "v1.$KUBEMINOR")
 
     GIT_TAG="v1.$KUBEMINOR.0-kw$((n+1))"
   else
-    git checkout --orphan $BRANCH
+    git checkout --orphan "$BRANCH"
     GIT_TAG="v1.$KUBEMINOR.0-kw1"
   fi
   rsync -av --exclude '.git' --delete-after "$OUT_DIR"/src/github.com/kubewarden/k8s-objects/ "$GIT_DIR"
   golangci-lint run ./...
-  git add -- *
+  git add .
   git commit -F "$GIT_COMMIT_MSG_FILE"
-  git tag -s -a -m "$GIT_TAG"  $GIT_TAG
+  git tag -s -a -m "$GIT_TAG" "$GIT_TAG"
   cd -
 done
